c17100dc75618fbea69a06150764dab6a38f1859
Nicolas-peter68 <71389477+Nicolas-peter68@users.noreply.github.com>
2020-10-23 16:54:32 +0200

    Premiers essais
    création du joueur et canvas, génération des mouvements

---
+++ anim.js
@@ -0,0 +1,31 @@
+let player = document.getElementById('player');
+let container = document.getElementById('container')
+
+console.log(player, container)
+let playerHaut = 0;
+let playerGauche = 0;
+
+
+function anime(e) {
+    if (e.keyCode == 39) {
+        playerGauche +=2;
+        player.style.left = playerGauche+'2px';
+    }
+
+    if(e.keyCode == 37){
+        playerGauche -=2;
+        player.style.left = playerGauche+'2px';
+    }
+
+    if(e.keyCode == 40){
+        playerHaut +=2;
+        player.style.top = playerHaut+'2px';
+    }
+
+    if(e.keyCode == 38){
+        playerHaut -=2;
+        player.style.top = playerHaut+'2px';
+    }
+}
+
+document.onkeydown = anime;
